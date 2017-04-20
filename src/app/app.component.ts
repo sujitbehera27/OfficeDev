200257b579e1c9e874261ab2fc1b6fad2d0aca8a
Yeshwanth <yeshwanth.alampalli@gmail.com>
2017-04-20 22:43:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  sectionInfo = "";
+  subSectionInfo = [];
+  mySource = {sections:['section1','section2','section3','section4','section5']}
+  subSource = {section1:['S01-SS01','S01-SS01','S01-SS01','S01-SS01','S01-SS01'],
+              section2:['S02-SS02','S02-SS02','S02-SS02','S02-SS02','S02-SS02'],
+              section3:['S03-SS03','S03-SS03','S03-SS03','S03-SS03','S03-SS03'],
+              section4:['S04-SS04','S04-SS04','S04-SS04','S04-SS04','S04-SS04'],
+              section5:['S05-SS05','S05-SS05','S05-SS05','S05-SS05','S05-SS05']}
+  subSubSource = {sections:['subSubSection-1','subSubSection-2','subSubSection-3','subSubSection-4','subSubSection-5']}
+  sectionSelected(val){
+    switch(val){
+      case this.mySource.sections[0]:
+        this.subSectionInfo = this.subSource.section1;
+        break;
+      case this.mySource.sections[1]:
+        this.subSectionInfo = this.subSource.section2;
+        break;
+      case this.mySource.sections[2]:
+        this.subSectionInfo = this.subSource.section3;
+        break;
+      case this.mySource.sections[3]:
+        this.subSectionInfo = this.subSource.section4;
+        break;
+      case this.mySource.sections[4]:
+        this.subSectionInfo = this.subSource.section5;
+        break;
+    }
+  }
+  title = 'Angular 2 nested Dropdown !';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
