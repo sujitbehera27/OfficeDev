a73716fd7a85aa5990e30ab4cf500b50a10e6624
Yeshwanth Alampalli <yeshwanth.alampalli@gmail.com>
2017-05-01 01:43:57 +0530

    Added Multi-select feature
    Added Multi-select feature

---
+++ src/app/filter/filter.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { IMultiSelectOption,IMultiSelectTexts,IMultiSelectSettings } from 'angular-2-dropdown-multiselect';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+export class FilterComponent implements OnInit {
+
+  constructor() {  }
+  optionsModel: number[];
+  myOptions: IMultiSelectOption[] = [
+      { id: 1, name: 'Section 1' },
+      { id: 2, name: 'Section 2' },
+      { id: 3, name: 'Section 3' },
+      { id: 4, name: 'Section 4' },
+      { id: 5, name: 'Section 5' },
+      { id: 6, name: 'Section 6' },
+  ];
+    // Settings configuration
+  mySettings: IMultiSelectSettings = {
+      enableSearch: true,
+      checkedStyle: 'fontawesome',
+      buttonClasses: 'btn btn-default btn-block',
+      dynamicTitleMaxItems: 3,
+      displayAllSelectedText: true
+  };
+
+  // Text configuration
+  myTexts: IMultiSelectTexts = {
+      checkAll: 'Select all',
+      uncheckAll: 'Unselect all',
+      checked: 'item selected',
+      checkedPlural: 'items selected',
+      searchPlaceholder: 'Find',
+      defaultTitle: 'Select',
+      allSelected: 'All selected',
+  };
+  onChange() {
+      console.log(this.optionsModel);
+  }
+  ngOnInit() {
+  }
+
+}
