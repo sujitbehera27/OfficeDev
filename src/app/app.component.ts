9fe42b753304574032b6abd408ac11eeb1ef4fa9
sujit <sujitbehera27@gmail.com>
2017-05-02 00:13:08 -0400

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,trigger,state,style,animate,transition } from '@angular/core';
+import { FilterComponent } from './filter/filter.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('filterPane' , [
+      state('inactive', style({
+        visibility: 'hidden',
+        display: 'none',
+        opacity: '0',
+        transition: 'visibility 0s, opacity 0.5s linear'
+      })),
+      state('active', style({
+        display: 'block',
+        transform: 'translateY(0%)',
+      })),
+      transition('inactive => active', animate(500)),
+      transition('active => inactive', animate(500))
+    ])
+  ]
 })
 export class AppComponent {
-  sectionInfo = "";
-  subSectionInfo = [];
-  mySource = {sections:['section1','section2','section3','section4','section5']}
-  subSource = {section1:['S01-SS01','S01-SS01','S01-SS01','S01-SS01','S01-SS01'],
-              section2:['S02-SS02','S02-SS02','S02-SS02','S02-SS02','S02-SS02'],
-              section3:['S03-SS03','S03-SS03','S03-SS03','S03-SS03','S03-SS03'],
-              section4:['S04-SS04','S04-SS04','S04-SS04','S04-SS04','S04-SS04'],
-              section5:['S05-SS05','S05-SS05','S05-SS05','S05-SS05','S05-SS05']}
-  subSubSource = {sections:['subSubSection-1','subSubSection-2','subSubSection-3','subSubSection-4','subSubSection-5']}
-  sectionSelected(val){
-    switch(val){
-      case this.mySource.sections[0]:
-        this.subSectionInfo = this.subSource.section1;
-        break;
-      case this.mySource.sections[1]:
-        this.subSectionInfo = this.subSource.section2;
-        break;
-      case this.mySource.sections[2]:
-        this.subSectionInfo = this.subSource.section3;
-        break;
-      case this.mySource.sections[3]:
-        this.subSectionInfo = this.subSource.section4;
-        break;
-      case this.mySource.sections[4]:
-        this.subSectionInfo = this.subSource.section5;
-        break;
-    }
-  }
-  title = 'Angular 2 nested Dropdown !';
+   header = ""
+   state: string = 'inactive'
+   filterToggle = false
+   mySource = {sections:['section1','section2','section3','section4','section5']}
+   private filterClick = () => {
+     this.state = (this.state === 'inactive' ? 'active' : 'inactive')
+     this.filterToggle = !this.filterToggle
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
+import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    Ng2AutoCompleteModule
+    Ng2AutoCompleteModule,
+    MultiselectDropdownModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
